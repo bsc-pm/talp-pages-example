00f1706973596581a7eae39108619166dfb5b7de
VALENTIN SEITZ <valentin.seitz@bsc.es>
2024-12-10 11:48:31 +0100

    Add NPB-MZ Benchmarks

---
+++ benchmarks/npb-mz/class_B/mestral/12x2/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=2
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+# We preload the LLVM-Runtime as it implements the GOMP API and has OMPT support, which we need for TALP-OpenMP
+export LD_PRELOAD="/usr/lib/llvm-19/lib/libomp-19.so.5"
+mpirun --allow-run-as-root -n 12  ../../../dlb.sh ../../../../../src/npb-mz/NPB3.4-MZ-MPI/bin/bt-mz.B.x
